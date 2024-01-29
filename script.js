4a3ca4c74818344747117def129709dfc61c6b11
KevinJMasson <kevin.j.masson@gmail.com>
2024-01-29 14:09:15 -0700

    Added Delete Button

--- script.js
+++ script.js
@@ -20,12 +20,24 @@ function createBook(event){
 
 function updateBookshelf () {
     bookShelf.innerHTML = "";
+    var i = 0;
     myLibrary.forEach(element => {
         const div = document.createElement('div');
         div.classList.add('book');
         div.textContent = element.title + "\r\n \r\n by " + element.author;
+        div.setAttribute("id", i.toString());
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", deleteBook);
+        div.appendChild(deleteButton);
         bookShelf.appendChild(div);
+        i++;
     });
+}
+
+function deleteBook(event) {
+    myLibrary.splice(parseInt(event.target.parentNode.id), 1);
+    updateBookshelf();
 }
 
 const bookShelf = document.querySelector("#bookshelf");
