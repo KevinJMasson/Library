8dd95e340374cd4107d3aa20a05b3298ef77144c
KevinJMasson <kevin.j.masson@gmail.com>
2024-01-29 11:20:28 -0700

    First push of base concept

---
+++ script.js
@@ -0,0 +1,35 @@
+const myLibrary = [];
+
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
+}
+
+function addBook(Book) {
+    myLibrary.push(Book);
+}
+
+function createBook(event){
+    let bookName = document.querySelector("#name");
+    let bookAuthor = document.querySelector("#author");
+    let newBook = new Book(bookName.value, bookAuthor.value);
+    addBook(newBook);
+    updateBookshelf();
+    event.preventDefault();
+}
+
+function updateBookshelf () {
+    bookShelf.innerHTML = "";
+    myLibrary.forEach(element => {
+        const div = document.createElement('div');
+        div.classList.add('book');
+        div.textContent = element.title + "\n by " + element.author;
+        bookShelf.appendChild(div);
+    });
+}
+
+const bookShelf = document.querySelector("#bookshelf");
+
+const submitButton = document.querySelector("#submitBook");
+
+submitButton.addEventListener("click", createBook);
